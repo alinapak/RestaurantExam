afd877b4ccc1367843cba4b01b9ee670378e4cad
alinapak <alina.pakamoryte@gmail.com>
2022-08-14 14:36:57 +0300

    authentication added

--- app/Http/Controllers/ApiControllers/ApiRestaurantController.php
+++ app/Http/Controllers/ApiControllers/ApiRestaurantController.php
@@ -9,10 +9,10 @@ use Illuminate\Http\Request;
 
 class ApiRestaurantController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth:api');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
     public function index()
     {
         return Restaurant::with('menu')->get();
